c925d0afb235e4fa6dca426fb541a0a8fa545b1a
Zitz59 <dondongudon@gmail.com>
2022-04-12 15:20:27 +0300

    07.Add UncontrolledAccordion component

---
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+
+type AccordionPropsType = {
+    titleValue: string
+    //collapsed: boolean
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function UncontrolledAccordion(props: AccordionPropsType) {
+    console.log('Accordion rendering')
+
+    let [collapsed, setCollapsed] = useState(false)
+
+    return <div>
+        <AccordionTitle title={props.titleValue}/>
+        <button onClick={() => {setCollapsed(!collapsed)}}>TOGGLE</button>
+        {!collapsed && <AccordionBody/>}
+    </div>
+
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log('AccordionTitle rendering')
+    {
+        return (
+            <h3>-- {props.title} --</h3>
+        )
+    }
+}
+
+function AccordionBody(){
+    console.log('AccordionBody rendering')
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
+
+
+export default UncontrolledAccordion;
