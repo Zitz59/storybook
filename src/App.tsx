417f8cf6b3288550afca12249bd32b2802f4ac8e
Zitz59 <dondongudon@gmail.com>
2022-04-27 20:28:08 +0300

    09.Callback. Add LocalState for controlled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Accordion from './components/Accordion/Accordion';
-import Rating from './components/Rating/Rating';
+import Rating, {RatingValueType} from './components/Rating/Rating';
 import OnOff from './components/OnOff/OnOff';
 import UncontrolledAccordion from './components/UncontrolledAccordion/UncontrolledAccordion';
 import UncontrolledRating from './components/UncontrolledRating/UncontrolledRating';
+import UncontrolledOnOff from './components/OnOff/UncontrolledOnOff';
 
-function App(props: any) {
+function App() {
     console.log('App rendering')
 
-    /*function isActive*/
+    let [ratingValue,setRatingValue] = useState<RatingValueType>(0)
+    let [accordionCollapsed,setAccordionCollapsed] = useState<boolean>(false)
+    let[switchOn,setSwitchOn] = useState(false);
+    let menuTitle = 'User'
 
     return (
+
         <div className="App">
-            {/*<Accordion titleValue={'Menu'} collapsed={true}/>
-            <Accordion titleValue={'Users'} collapsed={false}/>
-            <Rating value={1}/>
-            <Rating value={5}/>
-            <Rating value={3}/>*/}
-            <OnOff/>
+            <Accordion titleValue={menuTitle} collapsed={accordionCollapsed} onClick={setAccordionCollapsed}/>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+            <OnOff on={switchOn} onChange={(on)=>{setSwitchOn(on)}}/>
             <UncontrolledAccordion titleValue = {"Menu"}/>
             <UncontrolledAccordion titleValue = {"Users"}/>
             <UncontrolledRating/>
+            <UncontrolledOnOff/>
         </div>
     );
-}
-
-type PageTitlePropsType = {
-    title: string
-}
-
-function PageTitle(props: PageTitlePropsType) {
-    console.log('PageTitle rendering')
-    return <h1>{props.title}</h1>
 }
 
 
